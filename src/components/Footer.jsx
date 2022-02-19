14e5c29c8661573ee54d87f15173fd47584915d8
Adeodatus <adeoabdul.a@gmail.com>
2022-02-19 23:45:56 +0200

    Weather App Created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div className="">
+      <div className=" mt-12 py-3 px-10 border-t-2 border-red  mt-3 h-50 w-100 flex justify-center align-center">
+      <span>
+      <p className="flex items-center mb-10 text-lg"> Powred by Open Weather API </p> 
+      <span className=" text-white"> &copy;<em>2022 created by Adeodatus Abdul</em></span>
+      
+      </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
